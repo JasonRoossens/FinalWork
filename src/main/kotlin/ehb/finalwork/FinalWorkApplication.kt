cb23c080cf8aa3639ea1618075bacc18ecc5e038
JasonRoossens <jason.roossens@student.ehb.be>
2023-06-06 13:15:23 +0200

    Initial commit

---
+++ src/main/kotlin/ehb/finalwork/FinalWorkApplication.kt
@@ -0,0 +1,11 @@
+package ehb.finalwork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinalWorkApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinalWorkApplication>(*args)
+}
---
+++ src/test/kotlin/ehb/finalwork/FinalWorkApplicationTests.kt
@@ -0,0 +1,13 @@
+package ehb.finalwork
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinalWorkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
